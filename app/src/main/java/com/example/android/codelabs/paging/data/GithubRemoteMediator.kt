ee263f7b0f6f4a46425ac407118ba9010c712444
sundaydavid <sundaydavid989@gmail.com>
2020-09-28 10:10:31 +0100

    creating dataclass for keys

---
+++ app/src/main/java/com/example/android/codelabs/paging/data/GithubRemoteMediator.kt
@@ -0,0 +1,21 @@
+package com.example.android.codelabs.paging.data
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingState
+import androidx.paging.RemoteMediator
+import com.example.android.codelabs.paging.api.GithubService
+import com.example.android.codelabs.paging.db.RepoDatabase
+import com.example.android.codelabs.paging.model.Repo
+
+@OptIn(ExperimentalPagingApi::class)
+class GithubRemoteMediator(
+        private val query: String,
+        private val service: GithubService,
+        private val repoDatabase: RepoDatabase
+): RemoteMediator<Int, Repo>() {
+
+    override suspend fun load(loadType: LoadType, state: PagingState<Int, Repo>): MediatorResult {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/android/codelabs/paging/db/RemoteKeys.kt
@@ -0,0 +1,4 @@
+package com.example.android.codelabs.paging.db
+
+class RemoteKeys {
+}
